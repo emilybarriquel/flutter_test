73aafc7f00b7bca7a3ef4c0b979c49397350d92f
emilybarriquel <130797801+emilybarriquel@users.noreply.github.com>
2024-09-04 20:28:26 -0300

    Add files via upload

---
+++ meuapp/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\197705\\Downloads\\meuapp" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=C:\\Users\\197705\\Downloads\\meuapp"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\197705\\Downloads\\meuapp\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\197705\\Downloads\\meuapp"
+  "FLUTTER_TARGET=C:\\Users\\197705\\AppData\\Local\\Temp\\flutter_tools.7e3b8e9\\flutter_test_listener.32795064/listener.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,SU5URUdSQVRJT05fVEVTVF9TSE9VTERfUkVQT1JUX1JFU1VMVFNfVE9fTkFUSVZFPWZhbHNl"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\197705\\Downloads\\meuapp\\.dart_tool\\package_config.json"
+)
